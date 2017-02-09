be4693e8e5d236701bc4a4ccbc5cef4dde3b8d2a
Ignasi Marimon-Clos <ignasi35@gmail.com>
2017-02-09 13:02:58 +0100

    run messages thru topic·

--- helloworld-impl/src/main/java/sample/helloworld/impl/HelloServiceImpl.java
+++ helloworld-impl/src/main/java/sample/helloworld/impl/HelloServiceImpl.java
@@ -6,23 +6,19 @@ package sample.helloworld.impl;
 import akka.Done;
 import akka.NotUsed;
 import akka.japi.Pair;
-import akka.stream.javadsl.Flow;
 import com.lightbend.lagom.javadsl.api.ServiceCall;
 import com.lightbend.lagom.javadsl.api.broker.Topic;
 import com.lightbend.lagom.javadsl.broker.TopicProducer;
 import com.lightbend.lagom.javadsl.persistence.Offset;
 import com.lightbend.lagom.javadsl.persistence.PersistentEntityRef;
 import com.lightbend.lagom.javadsl.persistence.PersistentEntityRegistry;
-
-import java.util.Optional;
-
-import javax.inject.Inject;
-
 import sample.helloworld.api.GreetingMessage;
 import sample.helloworld.api.HelloService;
-import sample.helloworld.impl.HelloCommand.*;
+import sample.helloworld.impl.HelloCommand.Hello;
+import sample.helloworld.impl.HelloCommand.UseGreetingMessage;
 
-import static java.util.concurrent.CompletableFuture.completedFuture;
+import javax.inject.Inject;
+import java.util.Optional;
 
 /**
  * Implementation of the HelloService.
@@ -37,32 +33,22 @@ public class HelloServiceImpl implements HelloService {
   }
 
   @Override public ServiceCall<NotUsed, String> hello(String id) {
-    System.out.println("hello");
     return request -> {
-      // Look up the hello world entity for the given ID.
       PersistentEntityRef<HelloCommand> ref = persistentEntityRegistry.refFor(HelloWorld.class, id);
-      // Ask the entity the Hello command.
-      greetingsTopic();
       return ref.ask(new Hello(id, Optional.empty()));
     };
   }
 
   @Override public ServiceCall<GreetingMessage, Done> useGreeting(String id) {
-    System.out.println("usegreeting");
     return request -> {
-      // Look up the hello world entity for the given ID.
       PersistentEntityRef<HelloCommand> ref = persistentEntityRegistry.refFor(HelloWorld.class, id);
-      // Tell the entity to use the greeting message specified.
       return ref.ask(new UseGreetingMessage(request.message));
     };
 
   }
 
   @Override
-  //#implement-topic
   public Topic<GreetingMessage> greetingsTopic() {
-    System.out.println("greetingsTopic");
-    //throw new UnsupportedOperationException("Missing implementation");
     return TopicProducer.singleStreamWithOffset(offset -> persistentEntityRegistry.eventStream(HelloEventTag.INSTANCE, offset)
         .map(this::convertEvent));
   }
--- helloworld-impl/src/main/java/sample/helloworld/impl/Test.java
+++ helloworld-impl/src/main/java/sample/helloworld/impl/Test.java
@@ -1,27 +1,20 @@
 package sample.helloworld.impl;
 
-import static com.lightbend.lagom.javadsl.api.Service.named;
-import static com.lightbend.lagom.javadsl.api.Service.pathCall;
-
-import akka.Done;
 import akka.NotUsed;
 import com.lightbend.lagom.javadsl.api.Descriptor;
 import com.lightbend.lagom.javadsl.api.Service;
 import com.lightbend.lagom.javadsl.api.ServiceCall;
-import com.lightbend.lagom.javadsl.api.broker.Topic;
 
-import static com.lightbend.lagom.javadsl.api.Service.*;
+import static com.lightbend.lagom.javadsl.api.Service.named;
+import static com.lightbend.lagom.javadsl.api.Service.pathCall;
 
 public interface Test extends Service {
 
-  @Override
-  default Descriptor descriptor() {
-    // @formatter:off
-    return named("test").withCalls(
-        pathCall("/api/audit",  this::audit)
-    ).withAutoAcl(true);
-    // @formatter:on
-  }
-  // The topic handle
-  ServiceCall<NotUsed, NotUsed> audit();
+    @Override
+    default Descriptor descriptor() {
+        return named("test").withCalls(
+
+        ).withAutoAcl(true);
+    }
+
 }
